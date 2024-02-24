268d08666a9a8ab0ce52a5130c82f9d6e13b7431
Oryam <48219965+or-yam@users.noreply.github.com>
2024-02-24 21:42:35 +0200

    Add blog error screen

--- app/error.tsx
+++ app/error.tsx
@@ -1,22 +1,19 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
 import { useEffect } from "react";
 
 type Props = {
   error: Error & { digest?: string };
-  reset: () => void;
 };
 
-export default function Error({ error, reset }: Props) {
+export default function Error({ error }: Props) {
   useEffect(() => {
     console.error(error);
   }, [error]);
 
   return (
     <div className="flex flex-col justify-center items-center">
-      <h2>{"Couldn't get this blog 😞"}</h2>
-      <Button onClick={reset}>Try again</Button>
+      <h2>Something went wrong 😞</h2>
     </div>
   );
 }
