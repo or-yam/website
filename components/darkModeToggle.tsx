b125852b37b7b8e9220046dfb7e1d1b956a5222f
Oryam <48219965+or-yam@users.noreply.github.com>
2024-02-28 08:41:00 +0200

    Fix darkmode hydration issue

--- components/darkModeToggle.tsx
+++ components/darkModeToggle.tsx
@@ -3,18 +3,35 @@
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 import { Switch } from "@/components/ui/switch";
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 
 export const DarkModeToggle: FC = () => {
+  const [mounted, setMounted] = useState(false);
   const { setTheme, theme } = useTheme();
 
   const onCheckedChange = (isChecked: boolean) =>
     setTheme(isChecked ? "dark" : "light");
+  /** To avoid hydration mismatch https://www.npmjs.com/package/next-themes#avoid-hydration-mismatch */
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return (
+      <div className="flex items-center justify-center gap-2">
+        <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+        <Switch />
+      </div>
+    );
+  }
 
   return (
-    <div className="flex gap-2 justify-center items-center">
-      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <Moon className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+    <div className="flex items-center justify-center gap-2">
+      {theme === "light" ? (
+        <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      ) : (
+        <Moon className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      )}
       <Switch onCheckedChange={onCheckedChange} checked={theme !== "light"} />
     </div>
   );
